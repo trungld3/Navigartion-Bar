173e2f535237ed90765b15da80e82a82de35d389
trungld3 <k21tpm4@gmail.com>
2020-05-15 13:58:56 +0700

    Initial Commit

---
+++ Navigartion Bar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigartion Bar
+//
+//  Created by TrungLD on 5/15/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
